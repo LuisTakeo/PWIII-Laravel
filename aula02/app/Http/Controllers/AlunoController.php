bc3ee0dc1e183406a13a369da8b50558a7ae76ad
LuisTakeo <luistakeo@hotmail.com>
2023-08-03 22:34:32 -0300

    Aula sobre Laravel

---
+++ aula02/app/Http/Controllers/AlunoController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+// namespace == package
+use Illuminate\Http\Request;
+// use == import
+class AlunoController extends Controller
+{
+    //
+}
---
+++ camargo/resources/views/galeria.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main class="galeria">
+        <h1 class="galeria__title">Fotos da escola</h1>
+        <div class="galeria__images">
+            <img src="https://infoti.lwsite.com.br/uploads/asset/image/271/210/27121048/DSC01455.JPG" alt="">
+            <img src="https://bkpsitecpsnew.blob.core.windows.net/uploadsitecps/sites/1/2018/09/20130801.jpg" alt="">
+            <img src="https://www.each.usp.br/petsi/wp-content/uploads/2016/07/etec_camargo_aranha.jpeg" alt="">
+        </div>
+    </main>
+    
+</body>
+</html>
