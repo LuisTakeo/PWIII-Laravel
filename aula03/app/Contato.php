2c118a5e0a50838bfcfb5f7f1d51ad4df3da87d5
LuisTakeo <luistakeo@hotmail.com>
2023-08-10 22:30:19 -0300

    Aula sobre Laravel, criando controller, model e bd

---
+++ aula03/app/Contato.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+// php artisan make:model 
+class Contato extends Model
+{
+    protected $table = "tbContato";
+    
+}
---
+++ aula03/app/Http/Controllers/ContatoController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Contato;
+
+class ContatoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $contatos  = Contato::all();
+        // $contatos = Contato::where('idContato', '=', 1)->get();
+        // $contatos = Contato::where('nome', '<>', 'João')->get();
+        // Contato::where(nomeDoCampo, operador relacional, valorDaPesquisa)->get();
+
+        foreach($contatos as $contato){
+            echo "
+                Id: $contato->idContato <br/>
+                Nome: $contato->nome <br/>
+                Email : $contato->email <br/>
+                Assunto: $contato->assunto <br/>
+                Mensagem: $contato->mensagem <br/>
+            ";
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
